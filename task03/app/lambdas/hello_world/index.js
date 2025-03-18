9acc16a3efd77651eb7b37957ecc6f3750bc19bc
konga mamatha <konga_mamatha@epam.com>
2025-03-18 15:30:24 +0530

    task03

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,16 @@
+exports.handler = async (event) => {
+
+    // TODO implement
+
+    const response = {
+
+        statusCode: 200,
+
+        message: 'Hello from Lambda',
+
+    };
+
+    return response;
+
+};
+ 
